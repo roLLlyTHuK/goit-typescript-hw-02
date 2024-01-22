0d04999c1a3e148bbc09f8f34167a47ea3af68f1
roLLlyTHuK <ircvipua@gmail.com>
2024-01-22 12:04:51 +0200

    update answers

--- src/baseTypes/9.ts
+++ src/baseTypes/9.ts
@@ -2,7 +2,18 @@
   Створіть новий тип даних, який підходить для цих двох об'єктів.
 */
 
-const page1 = {
+type Page = {
+  title: string;
+  likes: number;
+  accounts: string[];
+  status: 'open' | 'close';
+  details?: {
+    createAt: Date;
+    updateAt: Date; // тут можно також створити опціональність updateAt?: Date; так як за логікою вона не обов'язкова, проте в page1 ця властивість присутня, тому я не створював
+  }
+}
+
+const page1 : Page = {
   title: 'The awesome page',
   likes: 100,
   accounts: ['Max', 'Anton', 'Nikita'],
@@ -13,7 +24,7 @@ const page1 = {
   }
 }
 
-const page2 = {
+const page2: Page = {
   title: 'Python or Js',
   likes: 5,
   accounts: ['Alex'],
